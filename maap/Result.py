1cd11b6e05781d757b8bad7e6e899855ce3e3682
bsatoriu <bsatoriu@jpl.nasa.gov>
2018-11-18 17:33:08 -0800

    first commit

---
+++ maap/Result.py
@@ -0,0 +1,66 @@
+import requests
+import shutil
+import urllib
+
+
+class Result(dict):
+    """
+    The class to structure the response xml string from the cmr API
+    """
+    _location = None
+
+    def download(self, destpath="."):
+        """
+        Download the dataset into file system
+        :param destPath: use the current directory as default
+        :return:
+        """
+        url = self._location
+        # Downloadable url does not exist
+        if not url:
+            return None
+        if url.startswith('ftp'):
+            urllib.urlretrieve(url,destpath + "/" + self._downloadname.replace('/', '') )
+        else:
+            r = requests.get(url, stream=True)
+            r.raw.decode_content = True
+
+            with open(destpath + "/" + self._downloadname.replace('/', ''), 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+
+    def getDownloadUrl(self):
+        """
+        :return:
+        """
+        return self._location
+
+class Collection(Result):
+    def __init__(self, metaResult, maap_host):
+        for k in metaResult:
+            self[k] = metaResult[k]
+
+        self._location = 'https://{}/search/concepts/{}.umm-json'.format(maap_host, metaResult['concept-id'])
+        self._downloadname = metaResult['Collection']['ShortName']
+
+class Granule(Result):
+    def __init__(self, metaResult):
+
+        for k in metaResult:
+            self[k] = metaResult[k]
+
+        # Retrieve downloadable url
+        try:
+            self._location = self['Granule']['OnlineAccessURLs']['OnlineAccessURL'][0]['URL']
+            self._downloadname = self._location.split("/")[-1]
+        except KeyError:
+            self._location = None
+
+        # Retrieve OPeNDAPUrl
+        try:
+            urls = self['Granule']['OnlineResources']['OnlineResource']
+            self._OPeNDAPUrl = filter(lambda x: x["Type"] == "OPeNDAP", urls)[0]['URL']
+        except :
+            self._OPeNDAPUrl = None
+
+    def getOPeNDAPUrl(self):
+        return self._OPeNDAPUrl
---
+++ maap/maap.py
@@ -0,0 +1,100 @@
+import logging
+import os
+import requests
+
+import xml.etree.ElementTree as ET
+from .Result import Collection, Granule
+from .xmlParser import XmlDictConfig
+
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser
+
+
+class MAAP(object):
+    def __init__(self, configFilePath=''):
+        """
+        :param configFilePath: The config file containing the credentials to make CRUD requests to CMR (extention .cfg)
+        These con
+        """
+        print(configFilePath)
+        self.config = ConfigParser()
+        if os.path.isfile(configFilePath) and os.access(configFilePath, os.R_OK ):
+            # Open the config file as normal
+            self.config.read(configFilePath)
+            self.configFilePath = configFilePath
+        else:
+            raise IOError("The config file can't be opened for reading")
+
+        self._PAGE_SIZE = self.config.getint("request", "page_size")
+        self._SEARCH_GRANULE_URL = self.config.get("request", "search_granule_url")
+        self._SEARCH_COLLECTION_URL = self.config.get("request", "search_collection_url")
+
+        self._CONTENT_TYPE = self.config.get("request", "content_type")
+        self._SEARCH_HEADER = {'Accept': self._CONTENT_TYPE}
+        self._MAAP_HOST = self.config.get("request", "maap_host")
+
+    def _get_search_results(self, url, limit, **kwargs):
+        """
+        Search the CMR granules
+        :param limit: limit of the number of results
+        :param kwargs: search parameters
+        :return: list of results (<Instance of Result>)
+        """
+        logging.info("======== Waiting for response ========")
+
+        page_num = 1
+        results = []
+        while len(results) < limit:
+            response = requests.get(
+                url=url,
+                params=dict(kwargs, page_num=page_num, page_size=self._PAGE_SIZE),
+                headers=self._SEARCH_HEADER
+            )
+            unparsed_page = response.text[1:-2].replace("\\", "")
+            #unparsed_page = response.content
+            page = ET.XML(unparsed_page)
+
+            empty_page = True
+            for child in list(page):
+                if child.tag == 'result':
+                    results.append(XmlDictConfig(child))
+                    empty_page = False
+                elif child.tag == 'error':
+                    raise ValueError('Bad search response: {}'.format(unparsed_page))
+
+            if empty_page:
+                break
+            else:
+                page_num += 1
+        return results
+
+    def searchGranule(self, limit=100, **kwargs):
+        """
+            Search the CMR granules
+
+            :param limit: limit of the number of results
+            :param kwargs: search parameters
+            :return: list of results (<Instance of Result>)
+            """
+        results = self._get_search_results(url=self._SEARCH_GRANULE_URL, limit=limit, **kwargs)
+        return [Granule(result) for result in results][:limit]
+
+    def searchCollection(self, limit=100, **kwargs):
+        """
+        Search the CMR collections
+        :param limit: limit of the number of results
+        :param kwargs: search parameters
+        :return: list of results (<Instance of Result>)
+        """
+        results = self._get_search_results(url=self._SEARCH_COLLECTION_URL, limit=limit, **kwargs)
+        return [Collection(result, self._MAAP_HOST) for result in results][:limit]
+
+
+if __name__ == "__main__":
+    m = MAAP("../maap.cfg")
+    print("initialized")
+    results = m.searchCollection(keyword='precipitation')
+    # Make sure that the XML response was actually parsed
+    valid = isinstance(results[0], Collection)
