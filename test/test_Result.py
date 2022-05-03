15bff7c1752fb010fb0811f755e0c0ff0f879b6c
Chuck Daniels <chuck@developmentseed.org>
2022-05-03 11:35:08 -0400

    Make Result.getData raise on failed HTTP download
    In addition:
    
    - Ignore Python-, OS-, and IDE-specific files
    - Remove accidentally committed checkpoint file
    - Tidy README.md formatting

---
+++ test/test_Result.py
@@ -0,0 +1,39 @@
+import pathlib
+import re
+
+import pytest
+import requests
+import responses
+
+from maap.Result import Granule
+
+GRANULE_BASE_URL = "https://data.mydaac.earthdata.nasa.gov"
+
+
+@responses.activate
+def test_getData_403_raises(tmp_path: pathlib.Path):
+    responses.get(
+        url=re.compile(f"{GRANULE_BASE_URL}/.*"),
+        status=403,
+        body="""
+            <?xml version="1.0" encoding="UTF-8"?>
+            <Error>
+                <Code>AccessDenied</Code>
+                <Message>Access Denied</Message>
+                <RequestId>REQUEST_ID</RequestId>
+                <HostId>HOST_ID</HostId>
+            </Error>
+            """,
+    )
+
+    url = f"{GRANULE_BASE_URL}/path/to/mydata"
+    granule = Granule(
+        metaResult={"Granule": {"OnlineAccessURLs": {"OnlineAccessURL": {"URL": url}}}},
+        awsAccessKey="",
+        awsAccessSecret="",
+        apiHeader={},
+        cmrFileUrl="",
+    )
+
+    with pytest.raises(requests.exceptions.HTTPError):
+        granule.getData(str(tmp_path))
