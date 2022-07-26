68c7a532c07575d29ede8dff244556fa822c23b6
Jamison French <jamisonjohnfrench@gmail.com>
2022-07-26 09:04:52 -0500

    Simplified raised response

--- maap/AWS.py
+++ maap/AWS.py
@@ -15,39 +15,30 @@ class AWS:
         self._s3_signed_url_endpoint = s3_signed_url_endpoint
         self._logger = logging.getLogger(__name__)
 
-    def requests_response(_url: str, _headers: object):
-        try:
-            response = requests.get(
-                url=_url,
-                headers=_headers
-            )
-
-            if response:
-                return json.loads(response.text)
-            response.raise_for_status()
-        except requests.exceptions.ConnectionError as e:
-            return ("Connection Error:", e)
-        except requests.exceptions.HTTPError as e:
-            return ("HTTP Error:", e)
-        except requests.exceptions.Timeout as e:
-            return ("Request Timeout:", e)
-        except requests.exceptions.TooManyRedirects as e:
-            return ("Redirects Error:", e)
-        except requests.exceptions.RequestException as e:
-            return ("Request Exception:", e)
-
     def requester_pays_credentials(self, expiration=60 * 60 * 12):
         headers = self._api_header
         headers['Accept'] = 'application/json'
 
-        return self.requests_response(self._requester_pays_endpoint + '?exp=' + str(expiration), self._api_header)
+        response = requests.get(
+            url=self._requester_pays_endpoint + '?exp=' + str(expiration),
+            headers=self._api_header
+        )
+        response.raise_for_status()
+
+        return json.loads(response.text)
 
     def s3_signed_url(self, bucket, key, expiration=60 * 60 * 12):
         headers = self._api_header
         headers['Accept'] = 'application/json'
         _url = self._s3_signed_url_endpoint.replace("{bucket}", bucket).replace("{key}", key)
 
-        return self.requests_response(_url + '?exp=' + str(expiration), self._api_header)
+        response = requests.get(
+            url=_url + '?exp=' + str(expiration),
+            headers=self._api_header
+        )
+        response.raise_for_status()
+
+        return json.loads(response.text)
 
     def earthdata_s3_credentials(self, endpoint_uri):
         headers = self._api_header
@@ -55,4 +46,10 @@ class AWS:
         _parsed_endpoint = urllib.parse.quote(urllib.parse.quote(endpoint_uri, safe=''))
         _url = self._earthdata_s3_credentials_endpoint.replace("{endpoint_uri}", _parsed_endpoint)
 
-        return self.requests_response(_url, self._api_header)
+        response = requests.get(
+            url=_url,
+            headers=self._api_header
+        )
+        response.raise_for_status()
+
+        return json.loads(response.text)
